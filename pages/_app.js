20d0f11c7091ec68e0778abec86e1f3e214ae83f
gpbsumido <gpbsumido@gmail.com>
2023-01-25 00:03:40 -0800

    add vercel audience analytics

--- pages/_app.js
+++ pages/_app.js
@@ -3,6 +3,7 @@ import { WagmiConfig, createClient } from 'wagmi'
 import { getDefaultProvider } from 'ethers'
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import { Analytics } from '@vercel/analytics/react';
 
  
 const client = createClient({
@@ -15,6 +16,7 @@ export default function App({ Component, pageProps }) {
     <WagmiConfig client={client}>
       <ToastContainer />
       <Component {...pageProps} />
+      <Analytics />
     </WagmiConfig>
   );
 }
