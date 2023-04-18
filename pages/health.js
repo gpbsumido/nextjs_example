0c49109d025b8e811a4d6d4b95f760243214ba76
gpbsumido <98932482+gpbsumido@users.noreply.github.com>
2023-04-18 12:42:55 -0700

    trial health

--- pages/health.js
+++ pages/health.js
@@ -1,38 +1,40 @@
+'use client'
+
 import React from "react";
 import { useState } from 'react';
-//import clientPromise from "../utils/mongo";
+import clientPromise from "../utils/mongo";
  
 export default function Health() {
   const [displayDiv,setDisplayDiv] = useState(<div></div>);
 
-  //async function getRestaurants(){
-  //  try {
-  //    const client = await clientPromise;
-  //    const db = client.db("sample_airbnb");
+  async function getRestaurants(){
+    try {
+      const client = await clientPromise;
+      const db = client.db("sample_airbnb");
     
-  //    const movies = await db
-  //      .collection("listingAndReviews")
-  //      .find({})
-  //      .sort({ _id: -1 })
-  //      .limit(20)
-  //      .toArray();
+      const movies = await db
+        .collection("listingAndReviews")
+        .find({})
+        .sort({ _id: -1 })
+        .limit(20)
+        .toArray();
     
-  //      // console.log( {
-  //      //     props: { movies: JSON.parse(JSON.stringify(movies)) },
-  //      // }) 
-  //    return movies;
-  //  } catch (e) {
-  //      console.error(e);
-  //  }
-  //}
+        // console.log( {
+        //     props: { movies: JSON.parse(JSON.stringify(movies)) },
+        // }) 
+      return movies;
+    } catch (e) {
+        console.error(e);
+    }
+  }
 
-  ////listener for esc ket close modal
-  //useEffect(() => {
-  //  getRestaurants().then(resp =>{
-  //    console.log(resp);
-  //    setDisplayDiv(<div>Displayed</div>)
-  //  });
-  //}, [isConnected]);
+  //listener for esc ket close modal
+  useEffect(() => {
+    getRestaurants().then(resp =>{
+      console.log(resp);
+      setDisplayDiv(<div>Displayed</div>)
+    });
+  }, []);
 
  
   return (
